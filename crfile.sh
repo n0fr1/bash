6acd648cf92a20596b30c66970f8da483c345b58
Evgeniy <n0fr1@mail.ru>
2023-02-08 19:20:14 +0300

    create file

---
+++ crfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# a simple creating file if doesn't exist
+current_date_time="`date "+%Y-%m-%d %H:%M:%S"`"
+PATH=/home/evgeniy/repo/bash/file.txt
+FILE=file.txt 
+if [ -f "$PATH" ]; then
+    echo "file already exists."
+else
+    echo $current_date_time > file.txt
+    echo $FILE was created
+fi
+
+
+
