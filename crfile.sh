fe9cdd554a281776fe922df26a3b1aaffa6de26b
Evgeniy <n0fr1@mail.ru>
2023-02-12 19:29:41 +0300

    new comment

--- crfile.sh
+++ crfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# a simple creating file if doesn't exist
+# a simple creating file if it doesn't exist
 current_date_time="`date "+%Y-%m-%d %H:%M:%S"`"
 PATH=/home/evgeniy/repo/bash/file.txt
 FILE=file.txt 
